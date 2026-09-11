9b2a16a318b8530482068d87c44038d1307a0f9d
caiodocurso-lab <caiodocurso@gmail.com>
2026-09-11 16:27:31 -0300

    add operador atribuição modulo

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totaldoces = 10;
+let crianças = 3;
+
+// queremos saber quantos doces sobram apos dividir igualmente entre as crianças
+totaldoces %= crianças; 
+
+console.log(totaldoces); // 1 -> pois 10 dividido por 3 sobra 1
