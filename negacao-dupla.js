3551670ca4642997590e703957f658268bffcf2c
caiodocurso-lab <caiodocurso@gmail.com>
2026-09-15 16:05:18 -0400

    add negação dupla

---
+++ negacao-dupla.js
@@ -0,0 +1,10 @@
+// valores falsy
+console.log(!!0);        
+console.log(!!"");        
+console.log(!!null);       0
+
+// Valores Trunthy
+console.log(!!1);        
+console.log(!! "texto");        
+console.log(!![]);        
+console.log(!!{});        
